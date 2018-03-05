5d2df85d55031972fa0f7c94c197cde66604d5be
tahmad121 <ahmadjava111@gmail.com>
2018-03-06 00:45:21 +0530

    After modifing

--- JavaJenkinsTest/src/hello/HelloWord.java
+++ JavaJenkinsTest/src/hello/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		for (int i = 1; i < 10; i++) {
 			System.out.println("HelloWord " + i);
+                        System.out.println("Hello How are you");
 		}
 	}
 }
